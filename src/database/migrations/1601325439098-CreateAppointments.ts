3d921bfc955d2d9581975e127228b2bce856b5a6
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-03 14:56:39 -0300

    created user model

--- src/database/migrations/1601325439098-CreateAppointments.ts
+++ src/database/migrations/1601325439098-CreateAppointments.ts
@@ -23,6 +23,16 @@ export default class CreateAppointments1601325439098 implements MigrationInterfa
                         name: 'date',
                         type: 'timestamp with time zone',
                         isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        default: 'now()'
                     }
                 ]
             })
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import {Entity, Column, PrimaryGeneratedColumn} from 'typeorm';
+import {Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn} from 'typeorm';
 
 @Entity('users')
 class User {
@@ -11,8 +11,14 @@ class User {
     @Column()
     email: string;
 
-    @Column('timestamp with time zone')
-    date: Date;
+    @Column()
+    password: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 }
 
 export default User;
