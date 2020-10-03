ae4915684d61a83d73c291950d839c4699530d56
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-03 15:37:08 -0300

    created basic user router to user sign up

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,7 +6,11 @@ const usersRouter = Router();
 
 usersRouter.post('/', async (request,response) => {
     try {
+        const {name, email, password} = request.body;
+        
+        
 
+        return response.send();
     } catch (err) {
         return response.status(400).json({error: err.message});
     }
