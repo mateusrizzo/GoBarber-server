7264462526bc8deec0aeb3f18cd90d769d649e07
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-03 18:12:47 -0300

    added authentication service to route

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,7 +10,12 @@ sessionsRouter.post('/', async (request,response) => {
     try {
         const {email, password} = request.body;
 
+        const authenticateUser = new AuthenticateUserService();
 
+        await authenticateUser.execute({
+            email,
+            password
+        });
 
         return response.json({ok: true});
     } catch (err) {
