d2b545adbb0f570dc0eb6cf3a01b465c2df85359
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-05 15:33:20 -0300

    Password del from return of avatar update route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -32,12 +32,14 @@ usersRouter.post('/', async (request,response) => {
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
     try {
         const updateUserAvatar = new UpdateUserAvatarService();
-        updateUserAvatar.execute({
+        const user = await updateUserAvatar.execute({
             user_id: request.user.id,
             avatarFilename: request.file.filename
         });
 
-        return response.json({ok: true});
+        delete user.password;
+
+        return response.json(user);
 
     } catch (err) {
         return response.status(400).json({error: err.message});
