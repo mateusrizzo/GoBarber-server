5fa5069a62c313f4043f306265e6237a106189e8
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-05 11:40:12 -0300

    Avatar creating route added

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,8 @@ import {Router} from 'express';
 
 import CreateUserService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 
 const usersRouter = Router();
 
@@ -20,6 +22,10 @@ usersRouter.post('/', async (request,response) => {
     } catch (err) {
         return response.status(400).json({error: err.message});
     }
+});
+
+usersRouter.patch('/avatar', ensureAuthenticated, async (request, response) => {
+    return response.json({ ok: true });
 });
 
 
