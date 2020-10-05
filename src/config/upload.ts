018223ec15a4e9b60414a6abdc6715309c30653d
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-05 14:48:00 -0300

    Image upload fixed

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,13 +3,14 @@ import crypto from 'crypto';
 import multer, { Options } from 'multer';
 
 export default <Options>{
-    strorage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'temp'),
-        filename (request, file, callback) {
-            const fileHash = crypto.randomBytes(10).toString('HEX');
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        filename(request, file, callback) {
+            const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;
 
             return callback(null, fileName);
         }
     }),
 }
+
