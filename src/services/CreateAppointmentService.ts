2aae1074515e09a47bf7708d76b83b4a739d730c
Mateus Rizzo <mateusrizzo13@gmail.com>
2020-10-05 15:54:38 -0300

    Error handling added

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -4,6 +4,8 @@ import {getCustomRepository} from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
+import AppError from '../errors/AppError';
+
 interface Request {
     provider_id: string;
     date: Date;
@@ -20,7 +22,7 @@ class CreateAppointmentService {
         const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
 
         if (findAppointmentInSameDate) {
-            throw Error('This date is already booked');
+            throw new AppError('This date is already booked');
         }
 
         const appointment = appointmentsRepository.create({
